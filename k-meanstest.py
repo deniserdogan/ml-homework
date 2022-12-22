92b095ba4b97816f993664e5a8b9af82559d2062
Дениз Эрдоган <denis@scraper.local>
2022-12-22 03:35:14 +0300

    optimal value k-means

--- k-meanstest.py
+++ k-meanstest.py
@@ -10,5 +10,4 @@ X, y = make_blobs(centers=7, n_samples=500, n_features=2,
 
 k = KMeans(maxK=8, max_iters=150, plot_steps=False)
 y_pred = k.predict(X)
-
-# k.plot()
+y_optimum = k.optimum(X)
--- kmeans.py
+++ kmeans.py
@@ -1,10 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial.distance import cdist
+from sklearn.cluster import KMeans as k_means
 
 np.random.seed(42)
 
+
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1-x2)**2))
+
 
 class KMeans:
 
@@ -13,9 +17,24 @@ class KMeans:
         self.max_iters = max_iters
         self.plot_steps = plot_steps
 
-    
         # mean feature vector for each cluster
         self.centroids = []
+
+    def optimum(self, X):
+        distortions = []
+        inertias = []
+        for K in range(1, self.maxK + 1):
+            kmeanModel = k_means(n_clusters=K).fit(X)
+            kmeanModel.fit(X)
+            distortions.append(sum(np. min(
+                cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+            inertias.append(kmeanModel.inertia_)
+
+        plt.plot(range(self.maxK), inertias, 'bx-')
+        plt.xlabel('Values of K')
+        plt.ylabel('inertias')
+        plt.title('The Elbow Method using Inertias For Optimal K')
+        plt.show()
 
     def predict(self, X):
         self.X = X
@@ -27,15 +46,16 @@ class KMeans:
             # list of sample indices for each cluster
             self.clusters = [[] for _ in range(K)]
 
-            #initialize centroids
-            random_sample_idxs = np.random.choice(self.n_samples, K, replace=False)
+            # initialize centroids
+            random_sample_idxs = np.random.choice(
+                self.n_samples, K, replace=False)
             self.centroids = [self.X[idxs] for idxs in random_sample_idxs]
 
             # optimization
             for _ in range(self.max_iters):
                 # update clusters
                 self.clusters = self._create_clusters(self.centroids, K)
-            
+
                 # update centroids
                 centroids_old = self.centroids
                 self.centroids = self._get_centroids(self.clusters, K)
@@ -45,15 +65,10 @@ class KMeans:
                     self.plot()
                     self.D.append(self.get_J(K))
                     break
-            # if self.get_D(self.D):
-            #     self.plot()
-            #     break
-            
-            
 
-            # return cluster labels
+        # return cluster labels
         return self._get_cluster_labels(self.clusters)
-        
+
     def _get_cluster_labels(self, clusters):
         labels = np.empty(self.n_samples)
         for cluster_idx, cluster in enumerate(clusters):
@@ -61,14 +76,13 @@ class KMeans:
                 labels[sample_idx] = cluster_idx
         return labels
 
-
     def _create_clusters(self, centroids, K):
         clusters = [[] for _ in range(K)]
         for idx, sample in enumerate(self.X):
             centroid_idx = self._closest_centroid(sample, centroids)
             clusters[centroid_idx].append(idx)
         return clusters
-    
+
     def _closest_centroid(self, sample, centroids):
         distances = [euclidean_distance(sample, point) for point in centroids]
         closest_idx = np.argmin(distances)
@@ -82,7 +96,8 @@ class KMeans:
         return centroids
 
     def _is_converged(self, centroids_old, centroids, K):
-        distances = [euclidean_distance(centroids_old[i], centroids[i]) for i in range(K)]
+        distances = [euclidean_distance(
+            centroids_old[i], centroids[i]) for i in range(K)]
         return sum(distances) == 0
 
     def plot(self):
@@ -97,23 +112,21 @@ class KMeans:
 
         plt.show()
 
-
-
     def get_J(self, j):
         sum = 0
 
         for i in range(j):
             for j in range(len(self.clusters[i])):
-                sum += np.sqrt((self.X[self.clusters[i]][j][0] - self.centroids[i][0]) **2 + (self.X[self.clusters[i]][j][0] - self.centroids[i][1])**2)
+                sum += np.sqrt((self.X[self.clusters[i]][j][0] - self.centroids[i][0]) ** 2 + (
+                    self.X[self.clusters[i]][j][0] - self.centroids[i][1])**2)
         return sum
 
     def get_D(self, array):
         res = []
         for i in range(1, len(array) - 1):
-            res.append(np.abs(array[i] - array[i+1]) / np.abs(array[i-1] - array[i]))
+            res.append(np.abs(array[i] - array[i+1]) /
+                       np.abs(array[i-1] - array[i]))
 
         for i in range(res):
             if res[i] == np.min(res):
                 return i
-        
-
